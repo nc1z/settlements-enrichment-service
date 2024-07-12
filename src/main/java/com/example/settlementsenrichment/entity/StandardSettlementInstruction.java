db721c010506253802058048c7fb8b244441b6ac
nc1z <ncxwork@gmail.com>
2024-07-12 21:56:17 +0800

    fix: account numbers are strings that can have leading zeros

--- src/main/java/com/example/settlementsenrichment/entity/StandardSettlementInstruction.java
+++ src/main/java/com/example/settlementsenrichment/entity/StandardSettlementInstruction.java
@@ -22,7 +22,7 @@ public class StandardSettlementInstruction {
 
     @NotNull(message = "payerAccountNumber cannot be null")
     @Column(name = "payer_account_number", nullable = false)
-    private final Long payerAccountNumber;
+    private final String payerAccountNumber;
 
     @NotNull(message = "payerBank cannot be null")
     @Column(name = "payer_bank", nullable = false)
@@ -31,7 +31,7 @@ public class StandardSettlementInstruction {
 
     @NotNull(message = "receiverAccountNumber cannot be null")
     @Column(name = "receiver_account_number", nullable = false)
-    private final Long receiverAccountNumber;
+    private final String receiverAccountNumber;
 
     @NotNull(message = "receiverBank cannot be null")
     @Column(name = "receiver_bank", nullable = false)
