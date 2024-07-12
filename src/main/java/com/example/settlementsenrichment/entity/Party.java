a2bb82c137adb1e4e00c603080e6757c7f8cae46
nc1z <ncxwork@gmail.com>
2024-07-12 23:06:05 +0800

    feat: Party class

---
+++ src/main/java/com/example/settlementsenrichment/entity/Party.java
@@ -0,0 +1,25 @@
+package com.example.settlementsenrichment.entity;
+
+import com.example.settlementsenrichment.util.ValidationConstants;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Party {
+    @NotNull(message = "Account number cannot be null")
+    @Pattern(regexp = "^[0-9]+$", message = "Invalid account number")
+    private String accountNumber;
+
+    @NotNull(message = "Bank code cannot be null")
+    @Pattern(regexp = ValidationConstants.SWIFT_CODE_REGEX, message = "Invalid SWIFT code")
+    private String bankCode;
+}
