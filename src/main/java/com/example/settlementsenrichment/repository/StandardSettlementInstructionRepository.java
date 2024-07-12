13aa975888334f2e4efcdbb13ce0225d6e36e86a
nc1z <ncxwork@gmail.com>
2024-07-12 21:44:06 +0800

    feat: StandardSettlementInstructionRepository

---
+++ src/main/java/com/example/settlementsenrichment/repository/StandardSettlementInstructionRepository.java
@@ -0,0 +1,7 @@
+package com.example.settlementsenrichment.repository;
+
+import com.example.settlementsenrichment.entity.StandardSettlementInstruction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StandardSettlementInstructionRepository extends JpaRepository<StandardSettlementInstruction, Long> {
+}
