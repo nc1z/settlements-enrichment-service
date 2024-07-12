cb15205f6ea2de6b7bbb4ebd9f77b21ad23291d1
nc1z <ncxwork@gmail.com>
2024-07-12 20:17:46 +0800

    feat: initial commit

---
+++ src/main/java/com/example/settlementsenrichment/SettlementsEnrichmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.settlementsenrichment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementsEnrichmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementsEnrichmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/settlementsenrichment/SettlementsEnrichmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.settlementsenrichment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementsEnrichmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
