665e97d42c2cca278a123f09468a5f156ef87f3f
nc1z <ncxwork@gmail.com>
2024-07-13 08:53:19 +0800

    TradeRequest Record

--- src/main/java/com/example/settlementsenrichment/dto/TradeRequest.java
+++ src/main/java/com/example/settlementsenrichment/dto/TradeRequest.java
@@ -1,4 +1,34 @@
 package com.example.settlementsenrichment.dto;
 
-public record TradeRequest() {
+import com.example.settlementsenrichment.util.ValidationConstants;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
+public record TradeRequest(
+        @JsonProperty("TradeId")
+        @NotBlank(message = "TradeId cannot be blank")
+        String tradeId,
+
+        @JsonProperty("SSI Code")
+        @NotBlank(message = "SSI Code cannot be blank")
+        String code,
+
+        @JsonProperty("Amount")
+        @NotNull(message = "Amount cannot be null")
+        @DecimalMin(value = "0.0", inclusive = false, message = "Amount must be greater than zero")
+        @Digits(integer = 15, fraction = 2, message = "Amount must have exactly 2 decimal places and at most 15 integer digits")
+        BigDecimal amount,
+
+        @JsonProperty("Currency")
+        @NotBlank(message = "Currency cannot be blank")
+        @Pattern(regexp = ValidationConstants.ISO4217_CURRENCY_CODE_REGEX, message = "Currency must be a valid ISO 4217 code")
+        String currency,
+
+        @JsonProperty("Value Date")
+        @NotBlank(message = "Value Date cannot be blank")
+        @Pattern(regexp = ValidationConstants.VALUE_DATE_REGEX, message = "Invalid value date, expected format is ddMMyyyy")
+        String valueDate
+) {
 }
--- src/main/java/com/example/settlementsenrichment/util/ValidationConstants.java
+++ src/main/java/com/example/settlementsenrichment/util/ValidationConstants.java
@@ -3,5 +3,6 @@ package com.example.settlementsenrichment.util;
 public class ValidationConstants {
     public static final String SWIFT_CODE_REGEX = "^[A-Z]{6}[A-Z0-9]{2}([A-Z0-9]{3})?$";
     public static final String NUMERIC_REGEX = "^[0-9]+$";
-    public static final String VALUE_DATE_REGEX = "\\d{8}";
+    public static final String VALUE_DATE_REGEX = "^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])\\d{4}$";
+    public static final String ISO4217_CURRENCY_CODE_REGEX = "[A-Z]{3}";
 }
