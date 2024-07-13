de4cb60628824ba2c5221f869b5cffd4ac7c89df
nc1z <ncxwork@gmail.com>
2024-07-13 10:34:57 +0800

    refactor: overload methods

--- src/main/java/com/example/settlementsenrichment/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/settlementsenrichment/exception/ResourceNotFoundException.java
@@ -4,4 +4,8 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String id) {
         super("Resource not found with ID: " + id);
     }
+
+    public ResourceNotFoundException(String message, String id) {
+        super(message + ": " + id);
+    }
 }
