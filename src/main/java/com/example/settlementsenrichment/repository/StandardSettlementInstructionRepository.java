7b738ba17c9ca79fc4acad2ad77aa01cf44c0965
nc1z <ncxwork@gmail.com>
2024-07-13 10:35:48 +0800

    feat: refactor and update repositories

--- src/main/java/com/example/settlementsenrichment/repository/StandardSettlementInstructionRepository.java
+++ src/main/java/com/example/settlementsenrichment/repository/StandardSettlementInstructionRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StandardSettlementInstructionRepository extends JpaRepository<StandardSettlementInstruction, Long> {
-    public Optional<StandardSettlementInstruction> findByCode(String code);
+    Optional<StandardSettlementInstruction> findByCode(String code);
 }
