90164c423d5adc33eeae62b0100bb637b5bc90ae
nc1z <ncxwork@gmail.com>
2024-07-13 22:42:32 +0800

    feat: created custom json deserializers

---
+++ src/main/java/com/example/settlementsenrichment/util/NonNumericStringOnlyDeserializer.java
@@ -0,0 +1,23 @@
+package com.example.settlementsenrichment.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public class NonNumericStringOnlyDeserializer extends JsonDeserializer<String> {
+
+    @Override
+    public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+            throws IOException, JsonProcessingException {
+        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+        if (node.isNumber()) {
+            String fieldName = jsonParser.currentName();
+            throw new IOException(String.format("Invalid type for field '%s'. Expected a string value.", fieldName));
+        }
+        return node.asText();
+    }
+}
---
+++ src/main/java/com/example/settlementsenrichment/util/NumericBigDecimalDeserializer.java
@@ -0,0 +1,24 @@
+package com.example.settlementsenrichment.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class NumericBigDecimalDeserializer extends JsonDeserializer<BigDecimal> {
+
+    @Override
+    public BigDecimal deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+            throws IOException, JsonProcessingException {
+        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+        if (!node.isNumber()) {
+            String fieldName = jsonParser.currentName();
+            throw new IOException(String.format("Invalid type for field '%s'. Expected a numeric value.", fieldName));
+        }
+        return new BigDecimal(node.asText());
+    }
+}
